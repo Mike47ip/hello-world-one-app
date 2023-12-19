3d847d5353b650785c2eaa69e6d31781d135cfcf
Mike Dark <agyemankojo47@gmail.com>
2023-12-19 02:43:22 +0000

    Fix rubocop soft fix

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,5 +1,5 @@
 class Api::GreetingsController < ApplicationController
   def random
-    render json: { greeting: Message.order("RANDOM()").first&.content }
+    render json: { greeting: Message.order('RANDOM()').first&.content }
   end
 end
